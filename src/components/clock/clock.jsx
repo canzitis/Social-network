8bcc0b7fb80d13dbb1aeed9d1ba582eabee7a779
canzitis <maks.zelenin22@gmail.com>
2021-07-05 22:13:51 +0300

    New CLOCK

--- src/components/clock/clock.jsx
+++ src/components/clock/clock.jsx
@@ -4,10 +4,12 @@ import s from "./clock.module.css";
 class Clock extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
       time: this.toOffsetDate(this.props.offset),
     };
   }
+
   componentDidMount() {
     this.intervalID = setInterval(() => this.tick(), 1000);
   }
@@ -16,17 +18,27 @@ class Clock extends React.Component {
   }
 
   toOffsetDate(offset) {
-    const d = new Date(new Date().getTime() + 3600 * 3000);
-    const hrs = d.getUTCHours();
-    const mins = d.getUTCMinutes();
-    return `${hrs}:${mins}`;
+    var days = [
+      "Воскресенье",
+      "Понедельник",
+      "Вторник",
+      "Среда",
+      "Четверг",
+      "Пятница",
+      "Суббота",
+    ];
+    const d = new Date();
+    const hrs = d.getHours();
+    const mins = d.getMinutes();
+    const numberDey = d.getDay();
+    return `${days[numberDey]}, ${hrs}:${mins}`;
   }
-
   tick() {
     this.setState({
       time: this.toOffsetDate(this.props.offset),
     });
   }
+
   render() {
     return <p className={s.clock}>{this.state.time}</p>;
   }
